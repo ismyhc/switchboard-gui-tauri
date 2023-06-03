2a47f9e03c67e6c213ab384fb7ab2052dcd1e3c8
Jacob Davis <ismyhc@gmail.com>
2023-06-03 09:38:07 -0400

    quick update to save work. Big changes coming

--- src/lib/components/ChainCardItemComponent.svelte
+++ src/lib/components/ChainCardItemComponent.svelte
@@ -4,12 +4,20 @@
   import { Command } from '@tauri-apps/api/shell';
   import { homeDir } from '@tauri-apps/api/path';
 
+  import { Buffer } from 'buffer';
+
   import { readDir, BaseDirectory, createDir } from '@tauri-apps/api/fs';
+
+  import { onMount } from 'svelte';
+
+  import { Body, fetch } from '@tauri-apps/api/http';
 
   export let title = '';
   export let description = '';
   export let other = '';
   export let disabled = true;
+
+  let chain_running = false;
 
   async function launch() {
     try {
@@ -22,7 +30,7 @@
     const homeDirPath = await homeDir();
 
     const args = [
-      `-regtest=true`,
+      `-regtest=1`,
       `-datadir=${homeDirPath}/.switchboard2/data/mainchain`,
       `-rpcport=18443`,
       `-rpcuser=user`,
@@ -30,9 +38,47 @@
       '-server=1'
     ];
     const command = Command.sidecar('binaries/drivechain-qt', args);
-    const output = await command.execute();
-    console.log(output);
+    console.log(command);
+    const output = await command.spawn();
+
+    while (!chain_running) {
+      await new Promise((r) => setTimeout(r, 1000));
+    }
+    console.log(chain_running);
   }
+
+  async function getblockcount() {
+    var userInfo = 'user:password';
+    var buf =
+      Buffer.from && Buffer.from !== Uint8Array.from ? Buffer.from(userInfo) : new Buffer(userInfo);
+    const res = await fetch('http://localhost:18443', {
+      method: 'POST',
+      headers: {
+        Authorization: `Basic ${buf.toString('base64')}`,
+        'content-type': 'application/json'
+      },
+      body: Body.json({
+        jsonrpc: '2.0',
+        id: 'switchboard',
+        method: 'getblockcount',
+        params: []
+      })
+    });
+    console.log(res.data);
+  }
+
+  async function update() {
+    try {
+      const res = await getblockcount();
+      chain_running = true;
+    } catch (error) {
+      chain_running = false;
+    }
+  }
+
+  onMount(async () => {
+    setInterval(update, 3000);
+  });
 </script>
 
 <div class="card-compact rounded-xl bg-base-100 {disabled ? 'opacity-25' : ''} shadow-offset-black">
